a495736d61a8efa812ff0641ad0b0a403c547366
Hohnn <jean-bateee@hotmail.fr>
2021-05-25 19:24:31 +0200

    style

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaG9obm4iLCJhIjoiY2twNDk2cDN1MDl3NzJ3czFvY2Joa2tsZiJ9.vEfQn-QQVpBmnM3sZCBnHw'
+}).addTo(mymap);
--- style-guide.md
+++ style-guide.md
@@ -16,7 +16,7 @@ The designs were created to the following widths:
 
 ### Body Copy
 
-- Font size (text input): 18px
+- Font size (text input): 
 
 ### Font
 
